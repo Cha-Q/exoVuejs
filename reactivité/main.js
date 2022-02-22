6ba6e5b668b62bd1e2dbfe9682a6aa14cedcaf83
Cha-Q <quentin.chassaing@outlook.com>
2022-02-22 10:09:00 +0100

    orthographe

--- reactivité/main.js
+++ reactivité/main.js
@@ -8,8 +8,8 @@ const monApp = Vue.createApp({
             uneString: 'Faites vos jeux !',
             horsJeu: 'display: ;',
             jeu: 'display:none;',
-            compteurV: 0,
-            compteurD: 0,
+            compteurV: '0 victoire',
+            compteurD: '0 défaite',
             cond: 'color: black',
             rep: '',
         };
