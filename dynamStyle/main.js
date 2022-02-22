77210eb8b9644ceedc770d1599bc55ebe3890507
Cha-Q <quentin.chassaing@outlook.com>
2022-02-22 13:35:57 +0100

    style

---
+++ dynamStyle/main.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+
+    data() {
+        return {
+
+            selectCard1: false,
+            selectCard2: false,
+
+        };
+    },
+
+    methods: {
+        selectionCard(uneCard) {
+            if (uneCard == 1) {
+                this.selectCard1 = !this.selectCard1;
+                console.log(this.selectCard2);
+                console.log(this.selectCard1);
+
+            } else if (uneCard == 2) {
+                this.selectCard2 = !this.selectCard2;
+                console.log(this.selectCard2);
+                console.log(this.selectCard1);
+            }
+        }
+
+    },
+
+});
+
+app.mount('#monApp')
