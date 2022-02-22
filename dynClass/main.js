fa62b0bf06c3537b2bc4c98256c54ae55ef7d345
Cha-Q <quentin.chassaing@outlook.com>
2022-02-22 14:07:56 +0100

    correction

---
+++ dynClass/main.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+
+    data() {
+        return {
+
+            selectCard1: false,
+            selectCard2: false,
+            nok: false,
+            ok: true
+
+        };
+    },
+
+    methods: {
+        selectionCard(uneCard) {
+            if (uneCard == 1) {
+                this.selectCard1 = !this.selectCard1;
+
+                console.log(this.selectCard1);
+
+            } else if (uneCard == 2) {
+                this.selectCard2 = !this.selectCard2;
+                console.log(this.selectCard2);
+                console.log(this.selectCard1);
+            }
+        }
+
+
+    },
+
+
+});
+
+app.mount('#monApp');
